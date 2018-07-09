c8c05ab45b53557ad881e9a46bcabbc60a3018b3
Rafael Jhonatan <rafaeljhonatan111@gmail.com>
2018-07-09 18:40:55 -0400

    Adciona class de exeception para tratamentos relacionados a task

---
+++ src/main/java/com/test/squadra/api/jobs/api/utils/exceptions/TaskExeception.java
@@ -0,0 +1,7 @@
+package com.test.squadra.api.jobs.api.utils.exceptions;
+
+public class TaskExeception extends Exception {
+    public TaskExeception(String message) {
+        super(message);
+    }
+}
